18e8168307071a014f5adc80d5feb1deabead251
baehyunki <unclepaper22@gmail.com>
2023-09-25 11:34:49 +0900

    css 파일 추가

--- 230925/lotto/script.js
+++ 230925/lotto/script.js
@@ -1,32 +1,46 @@
 // script.js
-console.log('running...')
+// console.log('running...')
+const button = document.getElementById("generate");
+const li = document.querySelectorAll("#result > li");
 
 // min: 최솟값, max: 최댓값을 이용한 난수생성
 function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 // 로또 번호 담을 배열
 let lottoNumber = []; // 6개
 
-for(let i = 0;lottoNumber.length < 6;i++){
-    // random 변수에 난수 생성한 값 할당
-    const random = randomNumber(1,45)
-    // lottoNumber 내부에 random과 일치하는 값이 있는지 true/false
-    const isEntry = lottoNumber.includes(random)
-    // !(not) 값을 뒤집어준다. true => false, false => true
-    // isEntry === true , !isEntry === false
-    !isEntry ? lottoNumber.push(random) : ''
+button.addEventListener("click", () => {
+  // 버튼 비활성화
+  button.classList.add("processing");
 
-    // if(isEntry){
-    //     '';
-    // } else { 
-    //     lottoNumber.push(random)
-    // }
-    // console.log(isEntry)
-    // 배열에 요소를 추가
-    console.log(lottoNumber)
-}
+  // 로또번호 초기화 : 로또 번호 길이 체크 후 비워주기 
+  if (lottoNumber.length > 0) {
+    lottoNumber = [];
+  }
 
+  // 로또번호를 생성하는 반복문
+  for (let i = 0; lottoNumber.length < 6; i++) {
+    const random = randomNumber(1, 45);
+    const isEntry = lottoNumber.includes(random);
+    !isEntry ? lottoNumber.push(random) : "";
+  }
 
-// 배열중에 '어떤값'이 존재하는지 유무에 따라 true, false
-lottoNumber.includes('어떤값')
+  // 오름차순 정렬
+  lottoNumber = lottoNumber.sort((a, b) => a - b);
+
+  // 처리 속도 변수
+  let timer = 200;
+
+  // 로또번호를 요소에 추가하는 반복문
+  for (let i = 0; i < lottoNumber.length; i++) {
+    setTimeout(() => {
+      li[i].textContent = lottoNumber[i];
+    }, timer * i);
+  }
+  
+  // 버튼 활성화
+  setTimeout(() => {
+    button.classList.remove("processing");
+  }, timer * lottoNumber.length);
+});
