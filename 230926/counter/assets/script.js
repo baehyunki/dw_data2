464e8104be177131fc258a480016239484a641b6
baehyunki <unclepaper22@gmail.com>
2023-09-26 12:33:38 +0900

    counter, forEach 문으로 변경

--- 230926/counter/assets/script.js
+++ 230926/counter/assets/script.js
@@ -4,20 +4,27 @@ console.log(dataList)
 // 반복문을 이용해서 dataList안에 있는 data-counter 값을 콘솔로 출력 
 
 // html : data-counter, js : element.dataset.counter
-for(let i = 0; i < dataList.length; i++){
-    const target = dataList[i].dataset.counter
-    const timerText = dataList[i].querySelector('.timer')
-
-    const step = Math.floor(target / 200)
-    console.log(step)
-
+// for(let i = 0; i < dataList.length; i++){
+//     let index = 0;
+//     const target = dataList[i].dataset.counter
+//     const timerText = dataList[i].querySelector('.timer')
+//     const step = Math.floor(target / 200)
+//     setInterval(() => {
+//         if(index < target){
+//             index = index + step       
+//             timerText.textContent = index
+//         }
+//     }, 1)
+// }
+dataList.forEach(item => {
     let index = 0;
+    const target = item.dataset.counter
+    const timerText = item.querySelector('.timer')
+    const step = Math.floor(target / 200)
     setInterval(() => {
         if(index < target){
-            // 증가
             index = index + step
-            // console.log(index)            
             timerText.textContent = index
         }
-    }, 1)
-}
+    }) 
+})
